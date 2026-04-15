380555d4920b716ede954fd1c69206b6f099ff56
RyosukeOkazaki-0408 <kamome0423@gmail.com>
2026-04-15 15:11:27 +0900

    -

--- 2nd/challenge/AlphameticValueTester.java
+++ 2nd/challenge/AlphameticValueTester.java
@@ -1,1 +1,18 @@
 // mainメソッドを含むAlphameticValueTesterクラスを書く
+public class AlphameticValueTester {
+   public static void main(String[] var0) {
+      AlphameticValue a = new AlphameticValue("ABCDEFGHI");
+      AlphameticValue b = new AlphameticValue("JJAIBICEF");
+      AlphameticValue c = new AlphameticValue("BIHJABDDE");
+
+      long start = System.nanoTime();
+      AlphameticValue.dfs(a, b, c);
+      long end = System.nanoTime();
+
+      int cnt = AlphameticValue.getCnt();
+      StringBuilder sb = AlphameticValue.getSb();
+      
+      sb.append("\n処理時間: ").append(String.format("%.3f", (double)(end - start) / 1E9)).append("秒").append("\n解の個数: ").append(cnt).append("個\n");
+      System.out.println(sb);
+   }
+}
